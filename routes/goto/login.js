94d46a410fce06e01258081665d6495c42e59c0b
svteen <svteen@users.noreply.github.com>
2014-10-27 19:23:48 +0800

    Update login.js

--- routes/goto/login.js
+++ routes/goto/login.js
@@ -1,43 +1,58 @@
 var express = require('express');
 var session = require('express-session');
-var mongojs = require('mongojs');
+//var mongojs = require('mongojs');
 var crypto = require('crypto');
+var mysql  = require('mysql');
 
 var router = express.Router();
 
-var databaseUrl = "moo";
-var collections = ['t_users'];
-var db = mongojs.connect(databaseUrl, collections);
+//var databaseUrl = "moo";
+//var collections = ['t_users'];
+//var db = mongojs.connect(databaseUrl, collections);
+
+//创建一个connection
+var connection = mysql.createConnection({     
+  host     : 'localhost',
+  user     : 'root',
+  password : '1111',
+  port: '3306',
+  database: 'vpster',
+}); 
+//创建一个connection
+connection.connect(function(err){
+    if(err){        
+      	console.log('[query] - :'+err);
+        return;
+    }
+      console.log('[connection connect]  succeed!');
+});  
+//执行SQL语句
+connection.query('SELECT * from tbl_user', function(err, rows, fields) { 
+     if(err){
+            console.log('[query] - :'+err);
+        return;
+     }
+     console.log('The tbl_user is: ', rows[0]);  
+});
 
 /* GET post listing. */
 router.get('/', function(req, res) {
-
-	//  if(req.session.islogin){
-	// console.log('usesession:' + req.session.islogin);
-	// res.locals.islogin = req.session.islogin;
-	//  }
   res.render('login', { title: '登陆页面' });
-  
 });
 
 router.post('/auth', function(req, res){
 	res.header("Access-Control-Allow-Origin", "http://localhost");
 	res.header("Access-Control-Allow-Methods", "GET, POST");
 	var jsonData = JSON.parse(req.body.mdata);
-	db.t_users.find({
+
+	/*db.t_users.find({
 		username : jsonData.username,
 		password : jsonData.password
 	}, function(err, doc){
 		console.log(doc);
-		if(doc.length > 0){
-			returnJson = '{"code" : "1101", "msg" : "sucess" }';
-			res.end(returnJson);
-		}else{
-			returnJson = '{"code" : "2022", "msg" : "failed, User not found!"}';
-			res.end(returnJson);
-		}
-	});
+	});*/
 	//res.redirect('/');
 });
 
 module.exports = router;
+
